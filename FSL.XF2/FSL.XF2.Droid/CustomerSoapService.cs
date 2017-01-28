2626799d79e98e4a409bcaf0491da50ac494cdbd
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2017-01-28 07:00:36 -0200

    publicacao do servico

--- FSL.XF2/FSL.XF2.Droid/CustomerSoapService.cs
+++ FSL.XF2/FSL.XF2.Droid/CustomerSoapService.cs
@@ -14,7 +14,8 @@ namespace FSL.XF2.Droid
         {
             service = new CustomersWs.Customers()
             {
-                Url = "http://10.0.2.2/FSL.WS/Customers.asmx"
+                Url = "http://codefinal.com/FSL.ConsummingAsmxServicesInXamarinForms/Customers.asmx" //remote server
+                //Url = "http://10.0.2.2/FSL.WS/Customers.asmx" //localserver - mobile does not understand "localhost", just that ip address
             };
         }
 
